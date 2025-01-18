2c51572605936247feda3957eb2adca0ec52b496
Alexey Khristolyubov <hristolyubov@flagsoft.ru>
2025-01-18 09:35:11 +0500

    Feat: adds localization to mission critical messages

---
+++ lang/ru/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'В нашей базе нет такого пользователя',
+    'password' => 'Неверный пароль',
+    'throttle' => 'Слишком много попыток входа. Подождите :seconds секунд и попробуйте ещё раз',
+
+];
---
+++ lang/ru/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'Вы успешно сбросили пароль',
+    'sent' => 'На ваш email отправлено письмо со ссылкой для сброса пароля',
+    'throttled' => 'Пожалуйста, подождите, прежде чем пробовать повторно сбросить пароль',
+    'token' => 'Некорректная ссылка для сброса пароля',
+    'user' => "Мы не можем найти пользователя с такой почтой",
+
+];
